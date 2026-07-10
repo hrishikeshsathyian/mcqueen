802c4a215e5625377b94a442ba4027a69eae2871
Hrishikesh Sathyian <e1121418@u.nus.edu>
2026-07-10 12:40:18 +0800

    Init supabase

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+from dotenv import load_dotenv
+load_dotenv()
+from postgrest.types import CountMethod
+
+from db.client import supabase
+
+response = (
+    supabase
+    .table("test")
+    .select("*", count=CountMethod.exact)
+    .execute()
+)
+print(response.data)
+print(response.count)
+
